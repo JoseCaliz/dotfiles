0e0eba2214efee7b77988ace0150e1a83f753794
JC <acalizv@gmail.com>
2021-06-10 14:18:28 -0500

    Copy scripts

---
+++ copy_from.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp -r ~/.tmux.conf ./tmux.conf
+cp -r ~/.config/nvim/init.vim .
+cp -r ~/Library/Preferences/com.googlecode.iterm2.plist .
---
+++ copy_to.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -r ./tmux.conf ~/.tmux.conf
+cp -r ./init.vim ~/.config/nvim/init.vim
+cp -r ./com.googlecode.iterm2.plist .~/Library/Preferences/com.googlecode.iterm2.plist
+cp -r ./development.sh ~/development.sh
