896c6a176e906dd24098752112468d3a4dad4cd6
JC <acalizv@gmail.com>
2021-06-21 10:53:10 -0500

    Not Passing iterminfo

--- copy_to.sh
+++ copy_to.sh
@@ -2,5 +2,5 @@
 
 cp -r ./tmux.conf ~/.tmux.conf
 cp -r ./init.vim ~/.config/nvim/init.vim
-cp -r ./com.googlecode.iterm2.plist .~/Library/Preferences/com.googlecode.iterm2.plist
+# cp -r ./com.googlecode.iterm2.plist .~/Library/Preferences/com.googlecode.iterm2.plist
 cp -r ./development.sh ~/development.sh
